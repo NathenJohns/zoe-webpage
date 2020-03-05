44669b60067dee08b9948fa7a7d6ee0d6827d588
Nathen Johns <nathenjohns@yahoo.co.uk>
2020-03-05 13:49:11 +0000

    emailjs setup

---
+++ js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "nathen", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.message.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            });
+}
